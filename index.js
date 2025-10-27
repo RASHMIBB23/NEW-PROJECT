e03596f6cc14113c09d6d14baaade7b14636f3a6
RASHMIBB23 <chandratejas0909@gmail.com>
2025-10-27 21:41:55 +0530

    this

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ app.get("/", (req, res) => {
 app.get("/news", (req, res) => {
   res.send({ status: 1, msg: "News Page API" });
 });
+app.get("/news/:id", (req, res) => {
+  let currentId = req.params.id;
+  res.send("News Details API" + currentId);
+});
 app.post("/login", (req, res) => {
   console.log(req.body);
-  res.send({ status: 1,
-     msg: "Login to the api", 
-     bodyData: req.body
-    queryData: req.data
-  
+  res.send({
+    status: 1,
+    msg: "Login to the api",
+    bodyData: req.body,
+    queryData: req.query,
+  });
 });
 app.listen("8000");
